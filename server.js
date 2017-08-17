de62b6bc2d04a95c044abce48649613a9d38bb28
Marcio <taboca@gmail.com>
2017-08-17 17:10:35 -0300

    Serverr to save screenshot

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var fs = require("fs"),
     http = require("http"),
     url = require("url"),
+    screenshot = require('electron-screenshot-service'),
     qs = require('querystring'),
     path = require("path");
 
@@ -14,6 +15,22 @@ app.post('/request', function(request, response){
   console.log(request.body);      // your JSON
   response.send(request.body);    // echo the result back
   response.send(200);
+
+  screenshot({
+    url : request.body.url,
+    width : 1024,
+    delay : 5000,
+    height : 768
+  })
+  .then(function(img){
+
+      //fs.writeFile('./out.png', img.data, function(err){
+      fs.writeFile(request.body.uuid+'.png', img.data, function(err){
+        screenshot.close();
+      });
+  });
+
+
 });
 
 app.listen(8888);
