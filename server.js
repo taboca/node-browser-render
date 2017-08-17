9d1844577231f3ae2d460bd903f9e3d3f86f9b20
Marcio <taboca@gmail.com>
2017-08-17 18:00:05 -0300

    Test receiving binary

--- server.js
+++ server.js
@@ -13,18 +13,31 @@ var mime = require('mime');
 
 app.use(bodyParser.json());
 
-app.post('/request', function(request, response){
+app.post('/request', function(request, res){
   console.log(request.body);      // your JSON
-  response.send(request.body);    // echo the result back
+  //response.send(request.body);    // echo the result back
 
   var webshot = require('webshot');
 
   webshot(request.body.url, request.body.uuid+'.png', function(err) {
     // screenshot now saved to google.png
+
+    var file = __dirname + '/taboca.png';
+
+    var filename = path.basename(file);
+    var mimetype = mime.lookup(file);
+
+    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+    res.setHeader('Content-type', mimetype);
+
+    var filestream = fs.createReadStream(file);
+    filestream.pipe(res);
+
   });
 
 });
 
+//https://stackoverflow.com/questions/7288814/download-a-file-from-nodejs-server-using-express
 app.get('/download', function(req, res){
 
   var file = __dirname + '/taboca.png';
@@ -37,6 +50,7 @@ app.get('/download', function(req, res){
 
   var filestream = fs.createReadStream(file);
   filestream.pipe(res);
+
 });
 
 app.listen(8888);
