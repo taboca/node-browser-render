a35ca452f6c40f6ade6c225d9a60974a16e67e25
Marcio <taboca@gmail.com>
2017-08-17 17:47:11 -0300

    Fix download

--- server.js
+++ server.js
@@ -23,4 +23,43 @@ app.post('/request', function(request, response){
 
 });
 
+app.get('/download', function(request, response){
+
+  var file = path.resolve(__dirname, "taboca.png");
+  fs.stat(file, function(err, stats) {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        // 404 Error if file not found
+        return res.sendStatus(404);
+      }
+    res.end(err);
+    }
+    var range = req.headers.range;
+    if (!range) {
+     // 416 Wrong range
+     return res.sendStatus(416);
+    }
+    var positions = range.replace(/bytes=/, "").split("-");
+    var start = parseInt(positions[0], 10);
+    var total = stats.size;
+    var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+    var chunksize = (end - start) + 1;
+
+    res.writeHead(206, {
+      "Content-Range": "bytes " + start + "-" + end + "/" + total,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "image/png"
+    });
+
+    var stream = fs.createReadStream(file, { start: start, end: end })
+      .on("open", function() {
+        stream.pipe(res);
+      }).on("error", function(err) {
+        res.end(err);
+      });
+  });
+}
+
+
 app.listen(8888);
